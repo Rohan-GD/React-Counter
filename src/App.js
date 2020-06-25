0c33f54116de2c11ffc29632926f426666cb9e21
Rohan George D <rohan.geo.2001@gmail.com>
2020-06-25 19:58:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Greet} from './components/greet'
+import Welcome from './components/welcome'
 
 function App() {
   return (
@@ -8,7 +10,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Greet  name="Rohan" lastname="George">
+            <p>I am an aspiring web developer</p>
+          </Greet>
+          <Welcome name="Rohan" lastname="George"/>
         </p>
         <a
           className="App-link"
@@ -16,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
       </header>
     </div>
