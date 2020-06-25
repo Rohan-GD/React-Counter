54a208477c71f7bf0b61f5d44932a7668d4dbae3
Rohan George D <rohan.geo.2001@gmail.com>
2020-06-25 20:34:52 +0530

    Updates

---
+++ precache-manifest.affb511acf7c26420605380a8be7c0be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac917b7de3c0b2eb887c9c6fc10c0d3",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "0f502b04d55125cc15ce",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9abb7f666e335c16461c",
+    "url": "/React-Deploy/static/js/2.d381c387.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Deploy/static/js/2.d381c387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f502b04d55125cc15ce",
+    "url": "/React-Deploy/static/js/main.bfdaa118.chunk.js"
+  },
+  {
+    "revision": "92ae304594cc1993b58a",
+    "url": "/React-Deploy/static/js/runtime-main.0e9f4c5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
